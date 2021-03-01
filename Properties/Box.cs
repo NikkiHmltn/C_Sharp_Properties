d3c01affe4706f919411208a728a3dd937f7eccf
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-03-01 09:44:38 -0800

    set up method to set and get lengths

--- Properties/Box.cs
+++ Properties/Box.cs
@@ -4,10 +4,21 @@ namespace Properties
     public class Box
     {
         //member variables
-        public int length;
+        // public technically makes it insecure code because everyone has access to it
+        private int length = 3;
         public int height;
         public int width;
         public int volume;
+
+        public void SetLength(int length)
+        {
+            this.length = length;
+        }
+
+        public int GetLength()
+        {
+            return this.length;
+        }
 
         public void DisplayInfo()
         {
--- Properties/Program.cs
+++ Properties/Program.cs
@@ -7,9 +7,15 @@ namespace Properties
         static void Main(string[] args)
         {
             Box box = new Box();
-            box.length = 3;
+            //setting box length cant because its private
+            //box.length = 3;
             box.height = 4;
             box.width = 5;
+
+            // getting the length is impossible too because its private
+            // set up a public method to set the length to work around it
+            box.SetLength(5);
+            Console.WriteLine("Box length is " + box.GetLength());
 
             box.DisplayInfo();
         }

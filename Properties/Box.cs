3a413a261eaf9be6f8304c036eecba026f2717c2
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-03-01 09:37:33 -0800

    set up class and instance of class box

---
+++ Properties/Box.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Properties
+{
+    public class Box
+    {
+        //member variables
+        public int length;
+        public int height;
+        public int width;
+        public int volume;
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Length is {0}, height is {1}, and the width is {2}, so the volume is {3}",
+                length, height, width, volume=length*height*width);
+        }
+    }
+}

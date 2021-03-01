87bb541eb44961ec078f9a83fb5d35fe7efe257f
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-03-01 10:33:47 -0800

    added notes about members

--- Properties/Box.cs
+++ Properties/Box.cs
@@ -67,5 +67,49 @@ namespace Properties
             Console.WriteLine("Length is {0}, height is {1}, and the width is {2}, so the volume is {3}",
                 length, height, Width, Volume=length*height*Width);
         }
+
+
+        //NOTES -----------------------------------
+
+        // MEMBER - PRIVATE FIELD
+        //private string memberName;
+        //private string jobTitle;
+        //private int salary;
+
+        //MEMBER - PUBLIC FIELD
+        //public int age;
+
+        //MEMBER - PROPERTY - EXPOSES JOBTITLE SAFETY - PROPS START WITH CAPITAL LETTER
+        //public string JobTitle { get; set; }
+
+        //PUBLIC MEMBER METHOD - CAN BE CALLED FOR OTHER CLASSES
+        // public void Introduce(bool isFriend){
+        //  Console.WriteLine("Hello, my name is {0}, and my job title is {1}", memberName, jobTitle)
+        // }
+
+        //PRIVATE MEMBER METHOD
+        // private void SharingInfo(){
+        //     Console.WriteLine("My salary is {0}", salary)
+        // }
+
+        // MEMBER CONSTRUCTOR
+        // public Members() {
+        //  age = 30;
+        //  memberName = "Lucy";
+        //  salary = 40000;
+        //  jobTitle = "Developer";
+        //  Console.WriteLine("Object Created")
+        // }
+
+        // MEMBER - FINALIZER - DESTRUCTOR
+        // TILDE IS VERY IMPORTANT ~
+        // ~Members(){
+        // ITS HERE TO CLEAN UP STATEMENTS
+        // 
+        // }
+
+
+
+
     }
 }
